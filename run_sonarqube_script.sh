72ebaf1de723162a50f81d1a90e95767ac831099
karthikstan11 <102356016+karthikstan11@users.noreply.github.com>
2024-02-05 12:14:08 +0530

    Update run_sonarqube_script.sh

--- run_sonarqube_script.sh
+++ run_sonarqube_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --net=host -v ${PWD}:/sonarqube-flask sonarsource/sonar-scanner-cli sonar-scanner -D sonar.projectBaseDir=/sonarqube-flask
+docker run --rm --net=host -v ${PWD}:/sonar sonar sonar-scanner -D sonar.projectBaseDir=/sonar
